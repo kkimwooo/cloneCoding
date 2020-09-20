ad130b056fa24e397b7f95c938a25b7a8073a074
Woo <kkimwooo@gmail.com>
2020-09-20 21:30:52 +0900

    Google, github 이용한 로그인

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -40,6 +40,19 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -66,8 +79,12 @@ const Auth = () => {
         {newAccount ? "Sign in" : "Create Account"}
       </span>
       <div>
-        <button>Google</button>
-        <button>github</button>
+        <button name="google" onClick={onSocialClick}>
+          Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Github
+        </button>
       </div>
     </div>
   );
